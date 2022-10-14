b1f65426f188c227f77f0d65ef075eab576245c6
Tatiana <tatianabohrer@gmail.com>
2022-10-14 17:01:16 -0400

    add query days

--- controllers/days.js
+++ controllers/days.js
@@ -1,5 +1,33 @@
-const showDay = (req, res) => {
-  res.render("");
+const { Pool } = require("pg");
+const HttpError = require("../models/http-error");
+
+const dbCredentials = {
+  user: process.env.DB_USERNAME,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  post: process.env.DB_PORT,
+};
+
+const getInterviewsPerDay = async (req, res, next) => {
+  const pool = new Pool(dbCredentials);
+  try {
+    const res = await pool.query(
+      "SELECT day_id,COUNT(appointment.*) AS appointments_count,COUNT(interview.*) AS interviews_count, day.time FROM appointment LEFT JOIN interview ON interview.appointment_id = appointment.id LEFT JOIN day ON day.id = appointment.day_id GROUP BY day_id, day.time;"
+    );
+    const result = res.rows;
+    console.log(result);
+
+    result.forEach((element) => {});
+  } catch (err) {
+    const error = new HttpError(
+      "Could not get any appointment on these days.",
+      500
+    );
+    return next(error);
+  } finally {
+    pool.end();
+  }
 };
 
 // make a request to the database
@@ -7,5 +35,3 @@ const showDay = (req, res) => {
 // calculate how many spots we have available for each day
 // return that to the client
 // example of the return in mocks - days.json
-
-module.exports = { showDay };
